348ce6f71762cc524321e433d9fe0c3c70021310
TokuyamaR <qpflapper.1007@gmail.com>
2022-09-20 23:09:32 +0900

    chore: LayoutコンポーネントのUI修正

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -1,19 +1,26 @@
+import styles from "./layout.module.css";
+import utilityStyles from "../../styles/utility.module.css";
 import Head from "next/head";
-import Image from "next/image";
 
-const name = "Tokuyama Code"
-const Layout = () => {
+const name = "Tokuyama Blog";
+
+const Layout = ({ children }) => {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico"/>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>
-        <Image src="/images/profile.jpeg" alt="プロフィール画像" />
-        <h1>{name}</h1>
+      <header className={styles.header}>
+        <img
+          src="/images/profile.jpeg"
+          alt="プロフィール画像"
+          className={`${styles.headerHomeImage} ${utilityStyles.borderCircle}`}
+        />
+        <h1 className={utilityStyles.heading2Xl}>{name}</h1>
       </header>
+      <main>{children}</main>
     </div>
   );
-}
+};
 
 export default Layout;
