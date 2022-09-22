4a8aac1106d6629ec02e2f059d81531aaeec1b7c
TokuyamaR <qpflapper.1007@gmail.com>
2022-09-22 13:56:44 +0900

    feat: 特定の記事データを取得する処理の追加

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,12 @@
 import Layout from "../../components/Layout/layout";
+import { getAllPostPaths } from "../../lib/getPost";
+
+export async function getStaticPaths() {
+  return {
+    paths: getAllPostPaths(),
+    fallback: false,
+  };
+}
 
 export default function post() {
   return <Layout>動的なルーティング設定によるページ</Layout>;
